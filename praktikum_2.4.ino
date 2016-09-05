b411f24b71ac9a6a36c6b8ea87200d52da42e3b1
Syifa Dzikri Tsani <syifadzikri@gmail.com>
2016-09-05 18:57:03 +0700

    Add files via upload

---
+++ praktikum_2.4.ino
@@ -0,0 +1,14 @@
+void setup(){
+  pinMode(A0, HIGH);
+  digitalWrite(A0, HIGH);
+  pinMode(12, OUTPUT);
+}
+void loop(){
+  if (digitalRead(A0)==LOW){
+    digitalWrite(12, HIGH);
+  }
+  else{
+    digitalWrite(12, LOW);
+  }
+}
+
